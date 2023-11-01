42460a5484754f07fec4f2b17a714a905885907e
maria <falcaomaria72@gmail.com>
2023-10-31 22:52:20 -0300

    feat: ajustes fontes

--- src/components/SectionExAlunos/BannerImageSlider/styles.ts
+++ src/components/SectionExAlunos/BannerImageSlider/styles.ts
@@ -14,8 +14,14 @@ export const BannerContainer = styled.div`
   gap: 1rem;
   border-radius: 39px;
   background: #ffc045;
-  padding: 2rem;
+  padding: 3rem;
   flex-direction: column;
+  @media (max-width: ${styleGuide.breakpoints.xsmall}) {
+    padding: 1rem 2rem 1rem 2rem;
+  }
+  @media (max-width: ${styleGuide.breakpoints.largesmall}) {
+    padding: 1rem 2rem 1.5rem 2rem;
+  }
 `;
 export const Image = styled.div<SliderBannerImage>`
   width: 80.185px;
@@ -36,27 +42,35 @@ export const Image = styled.div<SliderBannerImage>`
 `;
 export const Card = styled.div`
   margin-top: 1.25rem;
-  margin-left: 27px;
+  margin-left: 13px;
   @media (max-width: ${styleGuide.breakpoints.xsmall}) {
-    margin: auto;
+    margin-left: 0.5rem;
+    margin-top: 7px;
+  }
+  @media (max-width: ${styleGuide.breakpoints.largesmall}) {
+    font-size: 0.8rem;
   }
 `;
 export const Card1 = styled.div`
   display: flex;
+  margin-left: 1rem;
   @media (max-width: ${styleGuide.breakpoints.xsmall}) {
     display: flex;
+  }
+  @media (max-width: ${styleGuide.breakpoints.largesmall}) {
+    margin-left: 0.5rem;
   }
 `;
 export const Nome = styled.div`
   max-width: 350px;
   color: #181818;
-  font-size: 25px;
+  font-size: 20px;
   font-style: normal;
   font-weight: 800;
   line-height: normal;
   opacity: 0.5;
   @media (max-width: ${styleGuide.breakpoints.xsmall}) {
-    font-size: 1.2rem;
+    font-size: 1rem;
   }
   @media (max-width: ${styleGuide.breakpoints.largesmall}) {
     font-size: 1rem;
@@ -64,14 +78,14 @@ export const Nome = styled.div`
 `;
 export const Funcao = styled.div`
   color: #262525;
-  font-size: 19px;
+  font-size: 16px;
   font-style: normal;
   font-weight: 600;
   line-height: normal;
   max-width: 278px;
   opacity: 0.5;
   @media (max-width: ${styleGuide.breakpoints.xsmall}) {
-    font-size: 1.05rem;
+    font-size: 0.95rem;
   }
   @media (max-width: ${styleGuide.breakpoints.largesmall}) {
     font-size: 0.75rem;
@@ -80,15 +94,16 @@ export const Funcao = styled.div`
 export const Descricao = styled.div`
   max-width: 570px;
   color: #1e1e1e;
-  font-size: 28px;
+  font-size: 24px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
   opacity: 0.7;
+
   @media (max-width: ${styleGuide.breakpoints.xsmall}) {
-    font-size: 1.5rem;
+    font-size: 1.3rem;
   }
   @media (max-width: ${styleGuide.breakpoints.largesmall}) {
-    font-size: 1.2rem;
+    font-size: 1rem;
   }
 `;
